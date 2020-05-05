dca690e5f74722f649b20e372615f28678e53907
bamunson <46542850+bamunson@users.noreply.github.com>
2020-05-05 14:08:55 -0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='rcbtools',
-      version='0.1',
+      version='0.2',
       description='Analysis toolkit for RCB stars evolved in MESA',
       url='https://github.com/bamunson/rcbtools',
       author='Brad Munson',
